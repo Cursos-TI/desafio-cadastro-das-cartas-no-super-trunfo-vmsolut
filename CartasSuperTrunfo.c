5444a2cab60c9d5d70d09b2e5ae6d662ef398ec5
vmsolut <lucas@vmsolut.com.br>
2025-02-12 15:05:25 -0300

    teste1

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,6 +7,7 @@
 //Teste larissa
 
 int main() {
+    printf("Super Trunfo");
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     int codigoC, pontosT, populacao;
